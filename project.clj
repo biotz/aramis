5df7d390a459c7bf3be12629529bba3b750de865
Damian Hryniewicz <damian.hryniewicz@gmail.com>
2018-01-30 12:11:55 +0100

    change project.clj metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject aramis "0.1.1"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A library providing a Promise.all()-like capabilities for re-frame."
+  :url "https://github.com/magnetcoop/aramis"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
